b44ab315c290181bfde420fc7e884767c2c45a3b
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-22 13:49:16 +0000

    Add DayDay Logo to profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex flex-col justify-center items-center p-8">
+      <div className={styles.profile}>
+        <div className={styles.logo}>
+          <img
+            className="w-full h-full object-cover"
+            src="/images/DayDayLogoRebrand_2.png"
+            alt="DayDay Logo Brand"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
