72e523de0e8387e5f80f4e8b6d26d1a76fa87b63
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-22 13:55:00 +0000

    Add ProfileCard component to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./Profile.module.css";
+import ProfileCard from "../components/profileCard/ProfileCard";
 
 const Profile = () => {
   return (
     <div className="flex flex-col justify-center items-center p-8">
       <div className={styles.profile}>
+        {/* DayDay Logo section */}
+
         <div className={styles.logo}>
           <img
             className="w-full h-full object-cover"
@@ -12,6 +15,10 @@ const Profile = () => {
             alt="DayDay Logo Brand"
           />
         </div>
+
+        {/* Profile card section */}
+
+        <ProfileCard imgUrl="/images/Profile%20Image.png" />
       </div>
     </div>
   );
