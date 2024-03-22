694746d25b80372b746c570ef547d4c91c9868d1
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-22 14:36:09 +0000

    Add MixCard to profile page

--- app/components/profileBrief/ProfileBrief.tsx
+++ app/components/profileBrief/ProfileBrief.tsx
@@ -3,15 +3,15 @@ import styles from "./ProfileBrief.module.css";
 
 interface ProfileBriefProps {
   imgUrl: string;
-  alt: string;
+  imgAlt: string;
   text: string;
 }
 
-const ProfileBrief = ({ imgUrl, alt, text }: ProfileBriefProps) => {
+const ProfileBrief = ({ imgUrl, imgAlt, text }: ProfileBriefProps) => {
   return (
     <div className={styles.profileBrief}>
       <div className={styles.imgContainer}>
-        <img src={imgUrl} alt={alt} />
+        <img src={imgUrl} alt={imgAlt} />
       </div>
       <p className={styles.text}>{text}</p>
     </div>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,7 @@ import ProfileCard from "../components/profileCard/ProfileCard";
 import SocialCircles from "../components/socialCircles/SocialCircles";
 import ButtonHeader from "../components/buttonHeader/ButtonHeader";
 import ProfileBrief from "../components/profileBrief/ProfileBrief";
+import MixCard from "../components/mixCard/MixCard";
 
 const Profile = () => {
   return (
@@ -52,21 +53,21 @@ const Profile = () => {
         <div className={styles.container} style={{ marginBottom: "22px" }}>
           <ProfileBrief
             imgUrl="/images/1XTRA-image.png"
-            alt="1XTRA"
+            imgAlt="1XTRA"
             text="BBC 1XTRA Radio Shows"
           />
         </div>
         <div className={styles.container} style={{ marginBottom: "22px" }}>
           <ProfileBrief
             imgUrl="/images/Youtube%20Image%20Profile.png"
-            alt="Youtube profile"
+            imgAlt="Youtube profile"
             text="DJ DAY DAY Youtube Channel"
           />
         </div>
         <div className={styles.container}>
           <ProfileBrief
             imgUrl="/images/Merchandise%20Shop.png"
-            alt="DJ DAY DAY Merchandise"
+            imgAlt="DJ DAY DAY Merchandise"
             text="DJ DAY DAY Mechandise"
           />
         </div>
@@ -80,6 +81,18 @@ const Profile = () => {
           <ButtonHeader
             dotColour="#FF8600"
             text="NEW MIX LIVE - TAKE A LISTEN"
+          />
+        </div>
+
+        {/* Mix Card section */}
+
+        <div className={styles.container}>
+          <MixCard
+            imgUrl="/images/artworks-AjPHftHjPlxzkSxD-UuuZzg-t500x500.png"
+            imgAlt="mixtape artwork"
+            header="DJ DAY DAY"
+            text="@DJDAYDAY_ / The Vibe Mixtape Vol 6 (R&B, Hip Hop, Bashment, Afro Beats + UK Rap)"
+            time="19 hours ago"
           />
         </div>
       </div>
